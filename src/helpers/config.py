1e3dde72754d6bfb45927bb4f4f22e3e71c153b0
Mohamed Yahia <mohamed.yahia.kl@gmail.com>
2026-08-22 13:48:58 +0300

    chore: add gemini and serp api keys

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -4,6 +4,8 @@ class Settings(BaseSettings):
 
     DATABASE_URL: str
     EMBEDDING_DIMENSION: int
+    SERPAPI_API_KEY : str
+    GEMINI_API_KEY  : str
     model_config = SettingsConfigDict(        
         env_file= ".env",
     )
